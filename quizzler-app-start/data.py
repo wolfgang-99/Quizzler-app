5696e86b33ae3fb0076f8bbef68c37d573d65217
wolfgang-99 <56457199+wolfgang-99@users.noreply.github.com>
2023-04-29 03:39:01 -0700

    Add files via upload

---
+++ quizzler-app-start/data.py
@@ -0,0 +1,13 @@
+import requests
+
+parameter = {
+    "amount": 10,
+    "type": "boolean",
+    "difficulty": "medium"
+}
+response = requests.get(url="https://opentdb.com/api.php", params=parameter)
+response.raise_for_status()
+
+
+data = response.json()
+question_data = data["results"]
